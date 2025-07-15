2576a1eda5e797f76870749b7c15fc6fe648061a
K0K0-cod3s <etornam.koko@amalitech.com>
2025-07-15 07:54:59 +0000

    fixed

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { collection, query, where, onSnapshot } from 'firebase/firestore';
 import { db, appId } from '../config/firebase';
 import { Utensils, Dumbbell, TrendingUp, CheckCircle, Target, Flame, Activity, BarChart3, Database } from 'lucide-react';
